2b87d31fe29141b7eb7c667edb9ef51b3d64bb2b
quyl <quylai@gmail.com>
2019-11-14 18:32:15 -0600

    "save"

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const router = require("express").Router();
 import apiRoutes from "./api";
 
-export default function(app) {
-    apiRoutes(app);
+export default function (app) {
+	apiRoutes(app);
 
 };
