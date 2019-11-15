675c97f69ad1ec586a71d1c4ec37c9d310ed511a
quyl <quylai@gmail.com>
2019-11-14 23:23:08 -0600

    formatted spacing on SavedReelListItem comp. js file, document.cookie switching sequence causing 401-unauthorized

--- client/src/components/SavedReelListItem/index.js
+++ client/src/components/SavedReelListItem/index.js
@@ -8,36 +8,47 @@ import ReplayButton from "../ReplayButton";
 // RecipeList renders a bootstrap list item
 function SavedReelListItem (props) {
 
-    return (
-        <li>
-            <div className="saved-content">
-                <p className="time-stamp">{`Watched: ${props.timestamp}`}</p>
-                <div className="movie-content-container">
+	return(
+		<li>
+			<div className="saved-content">
+				<p className="time-stamp">{`Watched: ${props.timestamp}`}</p>
+				<div className="movie-content-container">
 
-                    <img className="movie-image" src={props.movieImage} alt={props.movieTitle}></img>
-                    <div className="movie-content-right">
-                        <h5 className="movie-title">{props.movieTitle}</h5>
+					<img className="movie-image" src={props.movieImage}
+						alt={props.movieTitle}></img>
+					<div className="movie-content-right">
+						<h5 className="movie-title">{props.movieTitle}</h5>
 
-                        <StarRating />
+						<StarRating />
 
-                        <p className="movie-synopsis"><span id="synopsis">Synopsis: </span>{props.movieSynopsis}</p>
-                
-                        <ul className="buttons">
-                            <li><ReplayButton /></li>
-                            <li><ShareButton /></li>
-                            <li><button id="delete-button" onClick={props.handleDelete}>DELETE</button></li>
-                        </ul>
-                    </div>
-                </div>
+						<p className="movie-synopsis">
+							<span id="synopsis">Synopsis: </span>
+							{props.movieSynopsis}
+						</p>
+		
+						<ul className="buttons">
+							<li><ReplayButton /></li>
+							<li><ShareButton /></li>
+							<li>
+								<button id="delete-button" onClick={props.handleDelete}>
+									DELETE
+								</button>
+							</li>
+						</ul>
+					</div>
+				</div>
 
-                <div className="saved-recipe">
-                    <p><span id="saved-recipe-head">Saved Recipe</span></p>
-                    <p><span id="saved-recipe"># Menu: </span>{props.recipeTitle}</p>
-                    <p><span id="saved-recipe"># Recipe: </span><a href={props.recipeLink} target="_blank">CLICK ME</a></p>
-                </div>
-            </div>
-        </li>
-    );
+				<div className="saved-recipe">
+					<p><span id="saved-recipe-head">Saved Recipe</span></p>
+					<p><span id="saved-recipe"># Menu: </span>{props.recipeTitle}</p>
+					<p>
+						<span id="saved-recipe"># Recipe: </span>
+						<a href={props.recipeLink} target="_blank">CLICK ME</a>
+					</p>
+				</div>
+			</div>
+		</li>
+	);
 }
 
 export default SavedReelListItem;
