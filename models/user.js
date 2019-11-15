d618c7cd69cb7aac4e19186d438daa974c2d4375
quyl <quylai@gmail.com>
2019-11-14 18:49:01 -0600

    reformat tabbing spacing, server-side

--- models/user.js
+++ models/user.js
@@ -3,38 +3,38 @@ var bcrypt = require("bcrypt");
 
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
+	const User = sequelize.define("User", {
 
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
 
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
 
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    hooks: {
-      beforeCreate: (user) => {
-        const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync(user.password, salt);
-      }
-    }
-  });
-  
-  User.prototype.validatePassword = function(password) {
-    return bcrypt.compareSync(
-      password,
-      this.password
-    );
-  };
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {
+		hooks: {
+			beforeCreate: (user) => {
+				const salt = bcrypt.genSaltSync();
+				user.password = bcrypt.hashSync(user.password, salt);
+			}
+		}
+	});
 
-  User.sync();
-  return User;
+	User.prototype.validatePassword = function (password) {
+		return bcrypt.compareSync(
+			password,
+			this.password
+		);
+	};
+
+	User.sync();
+	return User;
 };
--- routes/api/search.js
+++ routes/api/search.js
@@ -1,12 +1,11 @@
 const SearchController = require("../../controllers/SearchController");
 
-export default function(app) {
+export default function (app) {
 
-    app.route("/api/movie/:movie")
-    .get(SearchController.movieApi)
+	app.route("/api/movie/:movie")
+		.get(SearchController.movieApi)
 
-    app.route("/api/recipe/:recipe")
-    .get(SearchController.recipeApi)
+	app.route("/api/recipe/:recipe")
+		.get(SearchController.recipeApi)
 
 }
-
